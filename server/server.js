f83a92c010f7296d8443a8004b957f7dde5a4a27
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2024-10-10 15:15:08 +0900

    feat: 로그인 API 구현

--- server/server.js
+++ server/server.js
@@ -13,6 +13,43 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const port = 3001;
 
+app.post("/api/login", (req, res) => {
+  const { name, password } = req.body;
+
+  db.getConnection((err, connection) => {
+    if (err) {
+      console.log("DB connection error: ", err);
+      res.status(500).send("DB connection error");
+      return;
+    }
+    connection.query(
+      "select * from users where name = (?);",
+      [name],
+      async (error, result) => {
+        connection.release();
+
+        if (error) {
+          res.status(500).send("로그인 쿼리 실행 에러");
+          return;
+        }
+        if (result.length > 0) {
+          const isCorrectPassword = await bcrypt.compare(
+            password,
+            result[0].password
+          );
+          if (isCorrectPassword) {
+            res.json({ message: "로그인 성공" });
+          } else {
+            res.status(400).json({ message: "비밀번호 확인" });
+          }
+        } else {
+          res.status(400).json({ message: "존재하지 않는 사용자" });
+        }
+      }
+    );
+  });
+});
+
 // routing
 app.post("/api/signup", async (req, res) => {
   const { name, password } = req.body;
@@ -36,12 +73,12 @@ app.post("/api/signup", async (req, res) => {
           res.status(500).send("회원가입 실행 에러");
           return;
         }
-        if (result) {
+        if (result.affectedRows > 0) {
           console.log("result: ", result);
 
           // 쿼리 성공하면 리스폰스 보내고, 응답 종료
           res.json({
-            message: "로그인 성공!",
+            message: "회원가입 성공!",
             // userId: id, // 무슨아이디...?
           });
         }
