39ab4c14cc9dcd1fa3cb161e7d6b75525f944e49
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2024-10-14 19:44:29 +0900

    refactor: JWT 토큰 검증하는 미들웨어 구현

--- server/src/chattingRoom/routes.js
+++ server/src/chattingRoom/routes.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const chattingRoomController = require("./chattingRoomController");
+const authenticateUser = require("../middleware/authenticateUser");
 
 router.get("/", chattingRoomController.getChattingRooms);
-router.post("/", chattingRoomController.createChattingRoom);
-router.patch("/:roomId", chattingRoomController.updateChattingRoom);
+router.post("/", authenticateUser, chattingRoomController.createChattingRoom);
+router.patch(
+  "/:roomId",
+  authenticateUser,
+  chattingRoomController.updateChattingRoom
+);
 
 module.exports = router;
