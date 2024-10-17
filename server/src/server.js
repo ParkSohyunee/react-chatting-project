78b07e75dfa10d644729c1451aadb7e453f1e43c
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2024-10-17 12:41:21 +0900

    feat: (server) websocket을 사용하여 채팅 기능 구현(ws 라이브러리 설치)

--- server/src/server.js
+++ server/src/server.js
@@ -10,6 +10,12 @@ const chattingRouter = require("./chattingRoom/routes");
 
 const app = express();
 
+const WebSocket = require("ws");
+
+// Create an HTTP server and a WebSocket server
+const server = require("http").createServer(app);
+const ws = new WebSocket.WebSocketServer({ server });
+
 // middleware
 app.use(cors());
 app.use(bodyParser.json());
@@ -19,6 +25,36 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api/chattings", chattingRouter);
 
 const port = 3001;
+
+let sockets = [];
+
+ws.on("connection", (ws, req) => {
+  // 웹소켓 식별
+  ws.id = req.headers["sec-websocket-key"];
+  sockets.push(ws);
+
+  ws.on("close", (code, reason) => {
+    // console.log(code, reason);
+    sockets = sockets.filter((socket) => socket.id !== ws.id);
+    console.log(sockets.length);
+  });
+
+  // Broadcast the message to all connected clients
+  sockets.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(`new client connected!`);
+    }
+  });
+
+  // reply
+  ws.on("message", (message) => {
+    sockets.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message.toString());
+      }
+    });
+  });
+});
 
 // routing
 app.post("/api/login", async (req, res) => {
@@ -85,7 +121,7 @@ app.post("/api/signup", async (req, res) => {
   }
 });
 
-// server start
-app.listen(port, () => {
+// Start the WebSocket server
+server.listen(port, () => {
   console.log("서버가 3001번 포트에서 실행중");
 });
